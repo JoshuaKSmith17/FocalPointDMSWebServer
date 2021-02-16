f2dab17f7bdc9b8128a1b146e233b49db10da2b2
joshua Smith <joshuaksmith17@gmail.com>
2021-02-16 16:41:39 -0600

    Created Equipment class.

---
+++ FocalPointDMSWebServer/Models/Equipment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FocalPointDMSWebServer.Models
+{
+    public class Equipment
+    {
+        public long Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string SerialNumber { get; set; }
+    }
+}
