7a0b0f73f29a79bca10b73bfeaaf9730ffb744fd
joshua Smith <joshuaksmith17@gmail.com>
2021-02-15 19:14:49 -0600

    Registered FocalPointContext to the container.

--- FocalPointDMSWebServer/Startup.cs
+++ FocalPointDMSWebServer/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FocalPointDMSWebServer.Models;
 
 namespace FocalPointDMSWebServer
 {
@@ -28,10 +22,8 @@ namespace FocalPointDMSWebServer
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FocalPointDMSWebServer", Version = "v1" });
-            });
+                        
+            services.AddDbContext<FocalPointContext>(opt => opt.UseInMemoryDatabase("FocalPoint"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +32,7 @@ namespace FocalPointDMSWebServer
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FocalPointDMSWebServer v1"));
+               
             }
 
             app.UseHttpsRedirection();
