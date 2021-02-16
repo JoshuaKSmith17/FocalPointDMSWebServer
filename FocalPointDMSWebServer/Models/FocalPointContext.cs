07427a7cc602a43832755e1d94db37722f08f680
joshua Smith <joshuaksmith17@gmail.com>
2021-02-15 18:15:07 -0600

    Create the DbContext for project.

---
+++ FocalPointDMSWebServer/Models/FocalPointContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FocalPointDMSWebServer.Models
+{
+    public class FocalPointContext : DbContext
+    {
+        public FocalPointContext(DbContextOptions<FocalPointContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
