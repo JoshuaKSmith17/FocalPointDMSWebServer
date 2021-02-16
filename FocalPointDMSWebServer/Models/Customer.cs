86cc068204f4036dc2e2bc5afa112088acb95806
joshua Smith <joshuaksmith17@gmail.com>
2021-02-15 18:11:42 -0600

    Added Customer.  First entity to be represented by database.

---
+++ FocalPointDMSWebServer/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FocalPointDMSWebServer.Models
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+
+    }
+}
