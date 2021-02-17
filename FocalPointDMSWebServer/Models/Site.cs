a9fdcb5ddc00fa293b6cbab29578e6c8f1ba985b
joshua Smith <joshuaksmith17@gmail.com>
2021-02-17 00:52:53 -0600

    Created Site entity to represent a location owned and operated by a Customer entity.

---
+++ FocalPointDMSWebServer/Models/Site.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FocalPointDMSWebServer.Models
+{
+    public class Site
+    {
+        public long Id { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+
+    }
+}
