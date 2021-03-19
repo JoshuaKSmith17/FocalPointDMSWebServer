5c0b5d076358f95c05d300639f203392697b2960
joshua Smith <joshuaksmith17@gmail.com>
2021-03-19 16:48:25 -0500

    Added two new entities.

---
+++ FocalPointDMSWebServer/Models/RepairOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FocalPointDMSWebServer.Models
+{
+    public class RepairOrder
+    {
+        public long Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime CompletedDate { get; set; }
+        public List<RepairOrderLineItem> RepairOrderLineItem { get; set; }
+    }
+}
---
+++ FocalPointDMSWebServer/Models/RepairOrderLineItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FocalPointDMSWebServer.Models
+{
+    public class RepairOrderLineItem
+    {
+        public long Id { get; set; }
+        public string WorkPerformed { get; set; }
+        public Equipment Equipment { get; set; }
+    }
+}
